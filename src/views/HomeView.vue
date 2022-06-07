c4a80c84d0281cae1219bd4384ae472fe78fb372
hl_nathan <50773888+NathanHolanda@users.noreply.github.com>
2022-06-07 20:56:15 -0300

    banner

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,17 @@
 <template>
+  <Banner />
   <main class="main-container">
-    <h1>Home</h1>
+    <h1>Monte seu hambúrguer:</h1>
   </main>
 </template>
 
 <script>
+import Banner from "../components/Banner.vue"
 
 export default {
   name: 'HomeView',
   components: {
-  
+    Banner
   }
 }
 </script>
