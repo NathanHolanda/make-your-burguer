e89e384f853e91969726ddeb4f03788e81edab6f
hl_nathan <50773888+NathanHolanda@users.noreply.github.com>
2022-06-07 20:20:37 -0300

    header e footer

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <Navbar />
   <router-view/>
+  <Footer />
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: Helvetica, Arial, sans-serif;
+  }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  .main-container{
+    margin: 50px;
+    min-height: 250px;
+  }
 </style>
+
+<script>
+  import Navbar from "./components/Navbar.vue"
+  import Footer from "./components/Footer.vue"
+
+  export default {
+    components: {
+      Navbar,
+      Footer
+    }
+  }
+</script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <footer>
+        Make Your Burguer &copy; {{new Date().getFullYear()}}
+    </footer>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style scoped>
+    footer{
+        height: 200px;
+        background-color: #222;
+        border-top: 4px solid #111;
+        color: #fcba03;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+    }
+</style>
